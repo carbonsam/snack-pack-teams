ca998e280db625f530eb80b8655291876a561de7
Samuel Schumacher <samschu@live.com>
2019-05-13 20:18:44 -0500

    Add MS files

--- lambda/functions/messages/index.js
+++
@@ -1,7 +0,0 @@
-const { responseSuccess } = require('../../utilities/responses');
-
-export const receiveMessage = async (event, context) =>
-  responseSuccess({
-    type: 'message',
-    text: 'Hello user'
-  });
